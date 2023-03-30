a504d2a5c7861afb380ffd01ada95e3e2ff4ef4a
lgnvmaksim <lgnvmaksim@gmail.com>
2023-03-30 16:57:15 +0300

    done Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Profile} from "./Components/Profile";
+import {Login} from "./Components/Login";
+import {Error404} from "./Components/Error404";
+import {NewPasswordEnter} from "./Components/NewPasswordEnter";
+import {PasswordRecovery} from "./Components/PasswordRecovery";
+import {Registration} from "./Components/Registration";
 
 
 function App() {
     return <div>
-
+        <Routes>
+            <Route path={'profile'} element={<Profile/>}/>
+            <Route path={'registration'} element={<Registration/>}/>
+            <Route path={'login'} element={<Login/>}/>
+            <Route path={'password-recovery'} element={<PasswordRecovery/>}/>
+            <Route path={'new-password'} element={<NewPasswordEnter/>}/>
+            <Route path={'404'} element={<Error404/>}/>
+            <Route path={'*'} element={<Navigate to={'404'}/>}/>
+        </Routes>
     </div>
 }
 
--- src/Components/Error404.tsx
+++ src/Components/Error404.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Login = () => {
+export const Error404 = () => {
     return <div>
 
         </div>
